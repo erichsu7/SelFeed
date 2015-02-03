a3be7a51f65156aa7e8bb8cc559d19cc6bdda779
Eric Hsu <erichsu7@gmail.com>
2015-02-03 11:34:24 -0800

    Add User model

---
+++ SelFeed/app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+    def user_params
+    end
+end
---
+++ SelFeed/app/models/user.rb
@@ -0,0 +1,40 @@
+class User < ActiveRecord::Base
+  attr_reader: password
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil unless user
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  private
+    def ensure_session_token
+      self.session_token ||= self.class.generate_session_token
+    end
+end
