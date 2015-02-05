f64b6ad0c50bc9b7f8731cf2f2be43affb97dbb3
Eric Hsu <erichsu7@gmail.com>
2015-02-04 21:47:16 -0800

    Add picture preview after Cloudinary upload

--- app/assets/javascripts/views/pictures/picture_form.js
+++ app/assets/javascripts/views/pictures/picture_form.js
@@ -10,8 +10,22 @@ SelFeed.Views.PictureForm = Backbone.View.extend({
   render: function () {
     var renderedContent = this.template();
     this.$el.html(renderedContent);
+    this.activateCloudinary();
 
     return this;
+  },
+
+  activateCloudinary: function () {
+    $.cloudinary.config({cloud_name: "selfeed", api_key: "949363288817392"});
+    var that = this;
+    this.$('.cloudinary-widget').html($.cloudinary.unsigned_upload_tag("fuywh6de",
+      { cloud_name: 'selfeed' }));
+    this.$(".cloudinary_fileupload").on("cloudinarydone", function(event, data) {
+      var $img = $.cloudinary.image(data.result.public_id);
+      $img.addClass("picture-preview");
+      that.$(".cloudinary-widget").html($img);
+    })
+
   },
 
   createPicture: function (event) {
