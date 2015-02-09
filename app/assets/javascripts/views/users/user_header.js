d11b7c3a8553bf1ed7377ff80b4141bbe40d3dce
Eric Hsu <erichsu7@gmail.com>
2015-02-08 17:32:48 -0800

    Add user show header collage

--- app/assets/javascripts/views/users/user_header.js
+++ app/assets/javascripts/views/users/user_header.js
@@ -15,13 +15,14 @@ SelFeed.Views.UserHeader = Backbone.CompositeView.extend({
     var renderedContent = this.template({ user: this.model });
     this.$el.html(renderedContent);
     this.renderFollowButton();
+    this.renderCollage();
 
     return this;
   },
 
   toggleFollow: function (event) {
     event.preventDefault();
-    
+
     if (this.model.follow().isNew()) {
       this.model.follow().save();
     } else {
@@ -39,4 +40,24 @@ SelFeed.Views.UserHeader = Backbone.CompositeView.extend({
       this.$("#follow-user").html("Followed");
     }
   },
+
+  renderCollage: function () {
+    var user = this.model;
+    var collagePictures = [];
+    if (user.pictures().length > 0) {
+      while (collagePictures.length < 7) {
+        for (var i = 0; i < user.pictures().length; i++) {
+        collagePictures.push(user.pictures().models[i]);
+        }
+        i = 0;
+      }
+    }
+
+    var html = "";
+    for (i = 0; i < collagePictures.length - 1; i++) {
+      var url = collagePictures[i].escape("url");
+      html += "<img class=\"collage-picture\" id=\"picture-" + i + "\" src=\"" + url + "\">\n";
+    }
+    this.$(".user-header-collage-pictures").html(html);
+  }
 })
