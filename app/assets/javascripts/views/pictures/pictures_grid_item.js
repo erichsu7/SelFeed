3435fa152c02cdac49ba9db2bad324f66c98427d
Eric Hsu <erichsu7@gmail.com>
2015-02-09 11:24:58 -0800

    Add mouseover effect to picture grid

--- app/assets/javascripts/views/pictures/pictures_grid_item.js
+++ app/assets/javascripts/views/pictures/pictures_grid_item.js
@@ -4,9 +4,26 @@ SelFeed.Views.PicturesGridItem = Backbone.View.extend({
   className: "pictures-grid-item-container",
 
   render: function () {
+    var that = this;
     var renderedContent = this.template({ picture: this.model });
     this.$el.html(renderedContent);
+    this.addMouseover();
+    this.addMouseout();
 
     return this;
+  },
+
+  addMouseover: function () {
+    this.$el.on("mouseover", "img", ".grid-item-picture-details" ,function (event) {
+      var $div = $(event.delegateTarget).find(".grid-item-picture-details");
+      $div.addClass("hover-details");
+    });
+  },
+
+  addMouseout: function () {
+    this.$el.on("mouseout", "img", ".grid-item-picture-details" ,function (event) {
+      var $div = $(event.delegateTarget).find(".grid-item-picture-details");
+      $div.removeClass("hover-details");
+    });
   }
 })
