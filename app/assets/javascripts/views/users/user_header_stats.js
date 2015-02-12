7944bf33be9199d117183804058a7f6a3286fbe7
Eric Hsu <erichsu7@gmail.com>
2015-02-12 12:12:21 -0800

    Style header stats

--- app/assets/javascripts/views/users/user_header_stats.js
+++ app/assets/javascripts/views/users/user_header_stats.js
@@ -1,5 +1,6 @@
 SelFeed.Views.UserHeaderStats = Backbone.View.extend({
   template: JST["users/user_header_stats"],
+  className: "user-header-stats-list",
 
   render: function () {
     var renderedContent = this.template({ user: this.model });
